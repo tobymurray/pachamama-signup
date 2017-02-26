6e777dfadab383f62e10d305a8f284b92b2ae4ee
Toby Murray <murray.toby@gmail.com>
2017-02-26 11:59:24 -0500

    Add header

--- pacha-client/src/app/app.module.ts
+++ pacha-client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
