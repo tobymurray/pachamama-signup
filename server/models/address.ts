ed02ce55acf9cda2f534317931ffd06de435c6ab
Toby Murray <murray.toby@gmail.com>
2017-02-27 02:12:33 -0500

    Flesh out user and address models

--- server/models/address.ts
+++ server/models/address.ts
@@ -1,5 +1,32 @@
 export class Address {
   constructor(private line_1: string, private line_2: string, private line_3: string,
-    private city: string, private postal_code: string, private province: string, private country: string) {
+    private city: string, private postal_code: string, private province: string, private country: string, private _id?: number) {
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  static add(line_1: string, line_2: string, line_3: string, city: string,
+    postal_code: string, province: string, country: string) {
+    let now = new Date();
+    return (<any>global).knex('addresses').insert(
+      {
+        line_1: line_1,
+        line_2: line_2,
+        line_3: line_3,
+        city: city,
+        postal_code: postal_code,
+        province: province,
+        country: country,
+        created_at: now,
+        updated_at: now
+      }
+    ).returning('*')
+      .then(addresses => {
+        let address = addresses[0];
+        return new Address(address.line_1, address.line_2, address.line_3,
+          address.city, address.postal_code, address.province, address.country, address.address_id);
+      });
   }
 }
--- server/models/user.ts
+++ server/models/user.ts
@@ -1,5 +1,62 @@
+import { CryptoUtils } from '../utils/cryptoUtils';
+import { Address } from './address';
+
 export class User {
   constructor(private first_name: string, private last_name: string,
-    private phone_number: string, private email: string, private password: string) {
+    private phone_number: string, private email: string, private password: string, private _id?: number) {
   }
+
+  get id(): number {
+    return this._id;
+  }
+
+  addAddress(address: Address) {
+    return (<any>global).knex('customers_addresses').insert(
+      {
+        user_id: this.id,
+        address_id: address.id
+      });
+  }
+
+  static add(first_name: string, last_name: string, phone_number: string,
+    email: string, plaintextPassword: string) {
+    return CryptoUtils.encryptPassword(plaintextPassword)
+      .then(encryptedPassword => {
+        let now = new Date();
+        return (<any>global).knex('users').insert(
+          {
+            first_name: first_name,
+            last_name: last_name,
+            phone_number: phone_number,
+            email: email,
+            password: encryptedPassword,
+            created_at: now,
+            updated_at: now
+          }).returning('*');
+      }).then(users => {
+        let user = users[0];
+        return new User(user.first_name, user.last_name, user.phone_number, user.email, user.password, user.user_id);
+      });
+  }
+
+  static get(email: string) {
+    return (<any>global).knex('users').where({
+      email: email
+    }).then(users => {
+      return new Promise(function (resolve, reject) {
+        if (users.length === 0) {
+          return resolve(null);
+        }
+
+        if (users.length > 1) {
+          return reject("Found " + users.length + " users with email " + email);
+        }
+
+        let userData = users[0];
+        resolve(new User(userData.first_name, userData.last_name, userData.phone_number, userData.email, userData.password));
+      })
+    }).catch(error => {
+      console.error(error);
+    });
+  };
 }
