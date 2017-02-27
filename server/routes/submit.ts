b79250277864e49468b57105959cc11a1723603f
Toby Murray <murray.toby@gmail.com>
2017-02-27 02:13:41 -0500

    Hook up submit route to insert users and addresses

--- server/routes/submit.ts
+++ server/routes/submit.ts
@@ -1,10 +1,25 @@
 import express from 'express';
-var submit = express.Router();
+const submit = express.Router();
+
+import { User } from '../models/user';
+import { Address } from '../models/address';
 
 submit.post('/', function (req, res, next) {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ message: "success" }));
-  console.log(req.body);
+  let form = req.body;
+
+  Promise.all([
+    User.add(form['firstName'], form['lastName'], form['phone'], form['email'], form['password']),
+    Address.add(form['addressOne'], form['addressTwo'], '', form['city'], form['postalCode'], form['province'], form['country'])
+  ]).then(([user, address]) => {
+    return user.addAddress(address);
+  }).then(() => {
+    res.send(JSON.stringify({ message: "success" }));
+  }).catch(error => {
+    console.error(error);
+    res.send(JSON.stringify({ error: error }));
+  })
+
 });
 
 export { submit };
