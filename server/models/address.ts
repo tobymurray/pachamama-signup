fdd22601ff83bb311520f18eb211d48e81a6d497
Toby Murray <murray.toby@gmail.com>
2017-02-26 22:58:39 -0500

    Add model objects

---
+++ server/models/address.ts
@@ -0,0 +1,5 @@
+export class Address {
+  constructor(private line_1: string, private line_2: string, private line_3: string,
+    private city: string, private postal_code: string, private province: string, private country: string) {
+  }
+}
---
+++ server/models/user.ts
@@ -0,0 +1,5 @@
+export class User {
+  constructor(private first_name: string, private last_name: string,
+    private phone_number: string, private email: string, private password: string) {
+  }
+}
