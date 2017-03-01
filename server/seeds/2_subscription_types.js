73a688c9da52b2299c1cd1a63aeef1360b974d90
Toby Murray <murray.toby@gmail.com>
2017-02-28 19:59:15 -0500

    Add timestamps to subscription type seeds

--- server/seeds/2_subscription_types.js
+++ server/seeds/2_subscription_types.js
@@ -1,11 +1,16 @@
 exports.seed = function (knex, Promise) {
   return knex('subscription_types').del()
     .then(function () {
+      let now = new Date();
       return knex('subscription_types').insert([{
-          description: "Full share"
+          description: "Full share",
+          created_at: now,
+          updated_at: now
         },
         {
-          description: "Half share"
+          description: "Half share",
+          created_at: now,
+          updated_at: now
         }
       ]);
     });
