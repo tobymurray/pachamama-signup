a2eaf5ec14f8fd864ceb1cd9b71a2ff1b3f1a839
Toby Murray <murray.toby@gmail.com>
2017-03-05 01:17:23 -0500

    Move user model

--- server/users/user.ts
+++ server/users/user.ts
@@ -1,5 +1,5 @@
 import { CryptoUtils } from '../utils/cryptoUtils';
-import { Address } from './address';
+import { Address } from '../models/address';
 
 export class User {
   constructor(private first_name: string, private last_name: string,
