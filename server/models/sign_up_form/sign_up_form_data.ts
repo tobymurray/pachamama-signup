48f1c59ff37e41c17f2b59df95598ed4adf06006
Toby Murray <murray.toby@gmail.com>
2017-03-11 14:46:45 -0500

    Add interface for form data

---
+++ server/models/sign_up_form/sign_up_form_data.ts
@@ -0,0 +1,15 @@
+export interface SignUpFormData { 
+  firstName: string,
+  lastName: string,
+  phone: string,
+  email: string,
+  password: string,
+  addressOne: string,
+  addressTwo: string,
+  city: string,
+  postalCode: string,
+  province: string,
+  country: string,
+  shareSize: string,
+  pickUpLocation: string
+}
