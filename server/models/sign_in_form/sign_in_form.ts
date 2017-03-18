2740a8c667d608ecd0156fa6a7047f07e47102c1
Toby Murray <murray.toby@gmail.com>
2017-03-18 00:52:39 -0400

    Expand sign in to check password

--- server/models/sign_in_form/sign_in_form.ts
+++ server/models/sign_in_form/sign_in_form.ts
@@ -1,3 +1,4 @@
+import { CryptoUtils } from '../../utils/cryptoUtils';
 import { SignInFormData } from './sign_in_form_data';
 import { User } from './../users/user';
 
@@ -13,7 +14,17 @@ export class SignInForm {
     if (!trimmed_email) return Promise.reject("Email cannot be empty");
     if (!trimmed_password) return Promise.reject("Password cannot be empty");
 
-    return User.get(form.email);
+    return User.get(form.email)
+      .then(user => {
+        return CryptoUtils.encryptPassword(trimmed_password)
+          .then(encryptedPassword => {
+            if (CryptoUtils.passwordMatches(trimmed_password, encryptedPassword)) {
+              return user;
+            } else {
+              return Promise.reject("Username or password was incorrect");
+            }
+          });
+      });
   }
 
 }
